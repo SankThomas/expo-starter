b6295fdf5c9a4886b81c43d473343c9de3d5d0a8
SankThomas <thomassankara74@gmail.com>
2025-01-13 21:22:19 +0300

    update homepage

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,45 @@
-import { Image, StyleSheet, Platform } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { ScrollView, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <SafeAreaView className="flex-1 bg-white p-4">
+      <ScrollView>
+        <View>
+          <Text
+            style={{ fontFamily: "SpaceMono" }}
+            className="border-b-2 border-neutral-900 pb-4 text-center text-xl text-neutral-800"
+          >
+            Expo Template with TailwindCSS
+          </Text>
+          <Text style={{ fontFamily: "SpaceMono" }} className="my-6 text-base">
+            This template uses TypeScript and Nativewind (TailwindCSS for native
+            platforms).
+          </Text>
+          <Text style={{ fontFamily: "SpaceMono" }} className="mb-6 text-base">
+            I have added some default tabs to help me with the navigation when
+            building a new application.
+          </Text>
+          <Text style={{ fontFamily: "SpaceMono" }} className="mb-6 text-base">
+            The font is SpaceMono which is the default font in Expo. If you want
+            to change the font, simply add the font files in the{" "}
+            <Text className="bg-neutral-300">assets</Text> folder, and update
+            the code in the <Text className="bg-neutral-300">_layout.tsx</Text>{" "}
+            file.
+          </Text>
+          <Text style={{ fontFamily: "SpaceMono" }} className="mb-6 text-base">
+            Update the app name in the{" "}
+            <Text className="bg-neutral-300">app.json</Text> file.
+          </Text>
+          <Text
+            style={{ fontFamily: "SpaceMono" }}
+            className="mb-6 text-base underline"
+          >
+            Find me on YouTube, @tsbsankara. This is not a link. It is not
+            clickable.
+          </Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
