328017b068faa01ef8e83f81667d58c47d833d0d
SankThomas <thomassankara74@gmail.com>
2025-01-13 21:19:48 +0300

    tailwind config file

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all of your component files.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        green: "#148A09",
+        // Add more colors here
+      },
+    },
+  },
+  plugins: [],
+};
