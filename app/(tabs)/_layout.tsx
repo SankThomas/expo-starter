5c20caa1eeaa718cd3210db481d9c98b8f13b6a2
SankThomas <thomassankara74@gmail.com>
2025-01-14 09:12:17 +0300

    update tabBar styles

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -8,12 +8,18 @@ export default function TabLayout() {
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarShowLabel: false,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
             position: "absolute",
           },
-          default: {},
+          default: {
+            minHeight: 70,
+            borderTopWidth: 0,
+            borderTopColor: "#ffffff",
+            paddingTop: 15,
+          },
         }),
       }}
     >
